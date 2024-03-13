33c7f8ddc333d49d42e454b388c00cdcbfb32c1d
John Alejandro Agudelo Holguín <jhbr08@hotmail.com>
2024-03-13 16:35:08 -0500

    Scroll Button created

---
+++ src/components/CommentCard/ScrollButton.tsx
@@ -0,0 +1,31 @@
+import { Icon } from "@iconify/react";
+import React from "react";
+
+interface IScrollButton {
+  onPlusClick: () => void;
+  onMinusClick: () => void;
+}
+
+const ScrollButton: React.FC<IScrollButton> = ({
+  onPlusClick,
+  onMinusClick,
+}) => {
+  return (
+    <div className="flex flex-col rounded-[10px] bg-veryLightGray p-2 gap-2 text-center">
+      <div
+        className="p-1 cursor-pointer text-lightGrayishBlue hover:text-moderateBlue"
+        onClick={onPlusClick}
+      >
+        <Icon icon="icomoon-free:plus" className="text-sm" />
+      </div>
+      <div className="text-base text-moderateBlue font-bold">12</div>
+      <div
+        className="font-black p-1 cursor-pointer text-lightGrayishBlue hover:text-moderateBlue"
+        onClick={onMinusClick}
+      >
+        <Icon icon="icomoon-free:minus" className="text-sm" />
+      </div>
+    </div>
+  );
+};
+export default ScrollButton;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+const colors = require("tailwindcss/colors");
 
 const config: Config = {
   content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
@@ -9,6 +10,12 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+    },
+    colors: {
+      ...colors,
+      veryLightGray: "#F5F6FA",
+      lightGrayishBlue: "#C5C6EF",
+      moderateBlue: "#5357B6"
     },
   },
   plugins: [],
