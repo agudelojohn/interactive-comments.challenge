bb7bfa0d87cd0bbb2542258a8b39dfd074719734
John Alejandro Agudelo Holguín <jhbr08@hotmail.com>
2024-03-14 18:47:55 -0500

    Reply button added

--- src/components/CommentCard/ReplyButton.tsx
+++ src/components/CommentCard/ReplyButton.tsx
@@ -1,10 +1,24 @@
+import { Icon } from "@iconify/react";
 interface IReplayButton {
   className?: string;
+  type?: "primary" | "secondary";
 }
-const ReplyButton: React.FC<IReplayButton> = ({ className }) => {
+const ReplyButton: React.FC<IReplayButton> = ({
+  className,
+  type = "primary",
+}) => {
+  const btnType = type === "primary" ? "btn-primary" : "btn-secondary";
+  const btnText = "Reply";
   return (
-    <div className={className}>
-      <h1>ReplyButton</h1>
+    <div
+      className={`font-bold btn ${className} ${btnType} ${
+        type === "secondary" ? "w-[104px] h-[48px]" : ""
+      }`}
+    >
+      {type === "primary" && (
+        <Icon icon="icon-park-solid:back" className="text-lg" />
+      )}
+      {type === "primary" ? btnText : btnText.toUpperCase()}
     </div>
   );
 };
